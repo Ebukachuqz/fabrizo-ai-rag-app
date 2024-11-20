6b965aac43f97e4eb04b43489062035dd031065e
ebukachuqz <ebukachuqz@gmail.com>
2024-11-21 00:38:09 +0100

    Twitter scraper query edit for test

--- etl/scraper/main.py
+++ etl/scraper/main.py
@@ -8,7 +8,7 @@ async def main():
     CSV_FILENAME = os.path.join(os.getcwd(), "data", "fabrizioromano_tweets.csv")
 
     current_date = datetime.now().strftime("%Y-%m-%d")
-    QUERY = f'(from:FabrizioRomano) lang:en until:{current_date} since:2024-09-08 -filter:replies'
+    QUERY = f'(from:FabrizioRomano) lang:en until:2024-09-08 since:2024-09-06 -filter:replies'
 
     initialize_csv(CSV_FILENAME)
 
